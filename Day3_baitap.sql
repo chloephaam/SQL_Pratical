f6a645eaa00818d11471983462a3771200b6c394
chloephaam <155823228+chloephaam@users.noreply.github.com>
2024-01-07 00:06:55 +1100

    Update Day3_baitap.sql

--- Day3_baitap.sql
+++ Day3_baitap.sql
@@ -1,3 +1,17 @@
 --Bài tập ex1
 SELECT NAME FROM CITY
 WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA';
+--Bài tập ex2
+--Bài tập ex3
+--Bài tập ex4
+--Bài tập ex5
+--Bài tập ex6
+--Bài tập ex7
+--Bài tập ex8
+--Bài tập ex9
+--Bài tập ex10
+--Bài tập ex11
+--Bài tập ex12
+--Bài tập ex13
+--Bài tập ex14
+--Bài tập ex15
