db3d1d224726d16d66c70429930ab585f1f90c63
chloephaam <155823228+chloephaam@users.noreply.github.com>
2024-01-08 01:26:57 +1100

    Update Day5_pratical.sql

--- Day5_pratical.sql
+++ Day5_pratical.sql
@@ -13,14 +13,23 @@ FROM STATION;
 
 --Bài tập ex4
 SELECT
-ROUND (SUM (item_count*(order_occurrences))/ SUM(order_occurrences)) as MEAN_ORDER
+ROUND (CAST(SUM (item_count*(order_occurrences))/ SUM(order_occurrences) AS DECIMAL),1) as MEAN_ORDER
 FROM items_per_order;
 
 --Bài tập ex5
-
+SELECT candidate_id FROM candidates
+WHERE skill in ('Python', 'Tableau', 'PostgreSQL')
+GROUP BY (candidate_id)
+HAVING COUNT(skill) = 3
+ORDER BY candidate_id;
 
 --Bài tập ex6
-
+Select user_id,
+DATE (MAX (post_date)) - DATE (MIN (post_date)) as day_between
+from posts
+WHERE post_date >= '01-01-2021' AND post_date < '01-01-2022'
+GROUP by user_id
+Having COUNT (post_id) > 1
 
 --Bài tập ex7
 
