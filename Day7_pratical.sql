9f4c7356d7ac018607d9097764bae51ef8791a6d
chloephaam <155823228+chloephaam@users.noreply.github.com>
2024-01-09 19:13:51 +1100

    Update Day7_pratical.sql

--- Day7_pratical.sql
+++ Day7_pratical.sql
@@ -1,1 +1,34 @@
+--Bài tập ex1
+select Name from STUDENTS
+where Marks > 75
+order by Right (Name,3), ID;
+
+--Bài tập ex2
+select user_id,
+Upper(substring (name from 1 for 1)) || lower(substring (name,2)) as name
+from Users
+order by user_id
+
+--Bài tập ex3
+
+
+--Bài tập ex4
+
+
+--Bài tập ex5
+
+
+--Bài tập ex6
+
+
+--Bài tập ex7
+
+
+--Bài tập ex8
+
+
+--Bài tập ex9
+
+
+--Bài tập ex10
 
