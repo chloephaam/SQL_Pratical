b8611e5bf4e9cc1bcd846c6ebd57fc0c3f3a08ea
chloephaam <155823228+chloephaam@users.noreply.github.com>
2024-01-09 19:42:45 +1100

    Update Day7_pratical.sql

--- Day7_pratical.sql
+++ Day7_pratical.sql
@@ -17,10 +17,21 @@ GROUP BY manufacturer
 Order by total_sales DESC, manufacturer
 
 --Bài tập ex4
-
+SELECT product_id,
+Round(Avg (stars),2) as avg_stars,
+extract(month from submit_date) as mth
+FROM reviews
+Group by mth, product_id
+order by mth, product_id
 
 --Bài tập ex5
-
+SELECT sender_id,
+count (message_id) as total_msg
+FROM messages
+where (extract(month from sent_date) = 08)
+group by sender_id
+order by (total_msg) DESC
+limit 2
 
 --Bài tập ex6
 
