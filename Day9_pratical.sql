911385ddf38145342507a6c86d539a7c107038e1
chloephaam <155823228+chloephaam@users.noreply.github.com>
2024-01-10 19:34:52 +1100

    Update Day9_pratical.sql

--- Day9_pratical.sql
+++ Day9_pratical.sql
@@ -1,1 +1,9 @@
 
+SELECT
+Sum (CASE
+  when device_type = 'laptop' then 1 else 0
+end) as laptop_views,
+Sum (CASE
+  when device_type = 'laptop' or device_type = 'tablet' then 1 else 0
+end) as 	mobile_views
+FROM viewership;
