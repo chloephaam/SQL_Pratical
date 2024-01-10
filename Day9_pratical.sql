c68e2fd29f3ad45a9e1cf3cdb1618b49151653d0
chloephaam <155823228+chloephaam@users.noreply.github.com>
2024-01-10 19:35:53 +1100

    Update Day9_pratical.sql

--- Day9_pratical.sql
+++ Day9_pratical.sql
@@ -1,4 +1,4 @@
-
+--Bài tập ex1
 SELECT
 Sum (CASE
   when device_type = 'laptop' then 1 else 0
@@ -7,3 +7,14 @@ Sum (CASE
   when device_type = 'laptop' or device_type = 'tablet' then 1 else 0
 end) as 	mobile_views
 FROM viewership;
+
+--Bài tập ex2
+
+
+--Bài tập ex3
+
+
+--Bài tập ex4
+
+
+--Bài tập ex5
