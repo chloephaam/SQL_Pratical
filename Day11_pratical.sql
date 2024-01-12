3e9815a59c306ad100d122a32ee7277e65a78693
chloephaam <155823228+chloephaam@users.noreply.github.com>
2024-01-13 10:45:32 +1100

    Update Day11_pratical.sql

--- Day11_pratical.sql
+++ Day11_pratical.sql
@@ -13,10 +13,10 @@ count(distinct t2.email_id) / count(distinct t1.email_id) AS confirm_rate
 from emails as t1
 left join texts as t2 
 on t1.email_id = t2.email_id
-where t2.signup_action = 'Confirmed';
+where t2.signup_action = 'Confirmed'
 
 --Bài tập ex3
-select t2..age_bucket, 
+select t2.age_bucket, 
 round(100.0 * sum(case
     when t1.activity_type = 'send' then t1.time_spent alse 0 
 end)/ Ssum(t1.time_spent), 2) as send_perc, 
@@ -27,9 +27,15 @@ from activities as t1
 inner join age_breakdown AS t2 
 on t1.user_id = t2.user_id 
 where t1.activity_type in ('send', 'open') 
-group by t2.age_bucket;
+group by t2.age_bucket
 
 --Bài tập ex4
+select count(distinct t1.customer_id) AS supercloud_customers
+from customer_contracts as t1
+left join products as t2 
+on t1.product_id = t2.product_id
+group by t1.customer_id
+having count(distinct t2.product_category) = (select count(distinct product_category) from products)
 
 --Bài tập ex5
 
